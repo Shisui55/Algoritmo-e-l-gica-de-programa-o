4a2799757214615db9ba110a9acdf37b365684d0
Shisui <cauadasilva555@gmail.com>
2023-05-05 19:49:39 -0300

    05/05

---
+++ algoritmo/zero.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    //Faça um programa que leia um número e indique se é igual a zero
+    int num;
+
+    printf("Coloque um numero: ");
+    scanf("%d",&num);
+
+    if(num == 0)
+    {
+        printf("O numero e igual a zero");
+    }else{
+        printf("O numero nao e igual a zero");
+    }
+
+    return 0;
+}
