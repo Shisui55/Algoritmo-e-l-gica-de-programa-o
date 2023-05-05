98341c6f29ae0ad3dc8c6c82d5576965bc5916b7
Cauã Silva <117788495+Shisui55@users.noreply.github.com>
2023-05-05 10:33:12 -0300

    Aula_05/05

---
+++ gen_idade.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int q = 3,gen,quan_h;
+    float alt[12], media, maior = 0;
+    
+    for(int i = 0;i < q;i++)
+    {
+        printf("Digite a sua altura: ");
+        scanf("%f", &alt[i]);
+        
+        do{
+            printf("Digite o seu sexo([1]Masculino [2]Feminino): ");
+            scanf("%d", &gen);
+        }while(gen < 1 || gen > 2);
+        
+        media += alt[i];
+        
+        if(maior < alt[i] && gen == 2)
+        {
+            maior = alt[i];
+        }
+        if(gen == 1)
+        {
+            quan_h++;
+        }
+    }
+    printf("\nA maior altura das mulheres eh: %0.2f\nA Quantidade de homens eh: %d",maior, quan_h);
+}
